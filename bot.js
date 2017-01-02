ecce65f62461bf349c40cfcdca268f7af8f52dfc
Smoker-Ldr <donhbeattie@yahoo.com>
2017-01-02 13:20:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ function respond() {
       botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSlam = /^\/slam/; botRegexBrye = /^\/brye/;
       botRegexGian = /^\/gian/; botRegexScrotes = /^\/scrotes/; botRegexShaki = /^\/shaki/;
       botRegexDaf = /^\/dafuq/; botRegexAyy = /^\/ayy/; botRegexSchedule = /^\/schedule/; botRegexSteelers = /^\/steelers/; botRegexBengals = /^\/bengals/;
+      botRegexRaiders = /^\/raiders/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -173,6 +174,11 @@ function respond() {
   else if(request.text && botRegexBengals.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://www.realballersleague.com/stream/bengals/index.html");
+    this.res.end();
+  } 
+  else if(request.text && botRegexRaiders.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.realballersleague.com/stream/raiders/index.html");
     this.res.end();
   } 
   else if(request.text && botRegexSiege.test(request.text)) {
